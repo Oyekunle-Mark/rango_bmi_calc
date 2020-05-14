e6fecebfde613dc5ec1139de34655003d40ab762
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-14 12:31:19 +0100

    Extracted the icon content

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -25,25 +25,7 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       colour: activeCardColor,
-                      childCard: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(
-                            FontAwesomeIcons.mars,
-                            size: 80.0,
-                          ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Text(
-                            'MALE',
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              color: Color(0xff8e8e99),
-                            ),
-                          )
-                        ],
-                      ),
+                      childCard: IconContent(),
                     ),
                   ),
                   Expanded(
@@ -83,6 +65,35 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ));
+  }
+}
+
+class IconContent extends StatelessWidget {
+  const IconContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xff8e8e99),
+          ),
+        )
+      ],
+    );
   }
 }
 
