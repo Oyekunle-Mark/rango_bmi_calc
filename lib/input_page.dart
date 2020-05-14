eb100ec40194b1dca3997924c1ab5649469d3275
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-14 12:27:33 +0100

    Added a child card

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const borderContainerHeight = 80.0;
 const activeCardColor = Color(0xff1d1f33);
@@ -24,6 +25,24 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       colour: activeCardColor,
+                      childCard: Column(
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.mars,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Color(0xff8e8e99),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   Expanded(
