9741ba1de390064b54bb2fab7902d4deaf4392ff
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-15 14:20:15 +0100

    Calculates the bmi

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+  double _bmi;
+
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+}
