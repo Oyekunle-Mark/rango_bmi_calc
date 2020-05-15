3da3bf2bf45fc83a19e01b68d1c39464d98427eb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-15 14:41:22 +0100

    Converted from cm to m before calculating bmi

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -8,7 +8,7 @@ class CalculatorBrain {
   CalculatorBrain({this.height, this.weight});
 
   String calculateBMI() {
-    _bmi = weight / pow(height, 2);
+    _bmi = weight / pow(height / 100, 2);
     return _bmi.toStringAsFixed(1);
   }
 
