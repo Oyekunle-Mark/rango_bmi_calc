37e54ee0e3427a12590f6c4985fba3f960758890
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-15 10:24:42 +0100

    Created a new constant color

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,10 +4,11 @@ const kBorderContainerHeight = 80.0;
 const kActiveCardColor = Color(0xff1d1f33);
 const kInactiveCardColor = Color(0xff101427);
 const kBorderContainerColor = Color(0xffea1556);
+const kGrayColor = Color(0xff8e8e99);
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
-  color: Color(0xff8e8e99),
+  color: kGrayColor,
 );
 
 const kNumberTextStyle = TextStyle(
