5045e3308ec980ae79c4168e0bffecbaf696838d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-15 11:35:09 +0100

    Refactored

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
+import 'data_card_for_age_and_weight.dart';
 import 'icon_content.dart';
 import 'constants.dart';
 
@@ -170,73 +171,5 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ));
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Function onPressed;
-
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      shape: CircleBorder(),
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      elevation: 0.0,
-      onPressed: onPressed,
-      fillColor: Color(0xff4c4f5e),
-      child: Icon(icon),
-    );
-  }
-}
-
-class DataCardForAgeAndWeight extends StatelessWidget {
-  final String label;
-  final int value;
-  final Function onPressedMinus;
-  final Function onPressedPlus;
-
-  DataCardForAgeAndWeight(
-      {@required this.label,
-      @required this.value,
-      @required this.onPressedMinus,
-      @required this.onPressedPlus});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          label,
-          style: kLabelTextStyle,
-        ),
-        Text(
-          value.toString(),
-          style: kNumberTextStyle,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RoundIconButton(
-              icon: FontAwesomeIcons.minus,
-              onPressed: onPressedMinus,
-            ),
-            SizedBox(
-              width: 10.0,
-            ),
-            RoundIconButton(
-              icon: FontAwesomeIcons.plus,
-              onPressed: onPressedPlus,
-            ),
-          ],
-        ),
-      ],
-    );
   }
 }
