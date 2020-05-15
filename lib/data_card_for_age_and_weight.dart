2dde8d107143fdd8a8ca138e7b67c725a0251769
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-15 13:59:50 +0100

    Fixed a typo in the naming of the file

--- lib/data_card_for_age_and_weight.dart
+++ lib/data_card_for_age_and_weight.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'constants.dart';
-import 'rount_icon_button.dart';
+import 'round_icon_button.dart';
 
 class DataCardForAgeAndWeight extends StatelessWidget {
   final String label;
